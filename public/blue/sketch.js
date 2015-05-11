7510e283f7d35f329c29cb0f513996795a7fca4c
trodman <tristanrodman@gmail.com>
2015-05-11 14:13:35 -0400

    all sites user count networked

--- public/blue/sketch.js
+++ public/blue/sketch.js
@@ -2,8 +2,8 @@ var canvas; var sins; var cmajor; var socket;
 
 function setup() {
     cmajor = [0, 48, 50, 52, 53, 55, 57, 59, 60];
-    socket = io.connect('http://localhost:5000/');
-    //socket = io.connect('https://gradation.herokuapp.com');
+    //socket = io.connect('http://localhost:5000/');
+    socket = io.connect('https://gradation.herokuapp.com');
     socket.emit('blueConnect');
     $(this).unload(function() {
         socket.emit('blueDisconnect');
